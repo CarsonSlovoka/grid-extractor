1b93a68a67300465320ad960aa6d09b4d14fc337
Carson <jackparadise520a@gmail.com>
2020-09-15 13:21:06 +0800

    If window_size is -1, then the size is the same as the original.

--- grid_extractor/__init__.py
+++ grid_extractor/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ('ShadowClearMixin', 'GridExtractorBase', 'OCRMixin',
            'show_img')
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = ['Carson', ]
 __description__ = "It's a library to help you take out the text from the grid"
 
